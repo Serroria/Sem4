155fe69d1fb4b5051f782aca6b41a03890ca22c0
Merylien Jovanda <139000554+Serroria@users.noreply.github.com>
2025-03-27 15:58:26 +0700

    Add files via upload

---
+++ latihan/tugas5/script.js
@@ -0,0 +1,17 @@
+function rentangNilai(){
+    let nilai = document.getElementById("nilai").value;
+    let nilaiMutu;
+    if (nilai >=80 && nilai <=100){
+        nilaiMutu = "A";
+    }else if (nilai >=70 && nilai<=79){
+        nilaiMutu="B";
+    }else if (nilai >=60 && nilai <= 69){
+        nilaiMutu="C";
+    }else if(nilai >=50 && nilai<=59){
+        nilaiMutu="D";
+    }else if(nilai >=0 && nilai<=49){
+        nilaiMutu="E";
+    }
+    let hasil=document.getElementById("hasil").innerText = nilaiMutu;
+}
+console.log(rentangNilai());
