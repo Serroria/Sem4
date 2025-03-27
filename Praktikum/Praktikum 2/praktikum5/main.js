b9217ef1000653febd6e9370111e9ce4df03e9c6
Merylien Jovanda <139000554+Serroria@users.noreply.github.com>
2025-03-27 13:46:07 +0700

    Add files via upload

---
+++ Praktikum/Praktikum 2/praktikum5/main.js
@@ -0,0 +1,31 @@
+// alert('halo semua')
+// console.log('ini console')
+
+//const usia = 20;
+//usia = 21; error
+function salam(){
+    alert("Selamat datang di website kami!");
+}
+
+// function exampleLet(){
+//     let x = 10;
+//     if(true){
+//         let y = 20;
+//         console.log("Nilai dalam blok:",y);
+//     }
+//     console.log("Nilai luar blok:", y);
+// }
+// exampleLet();
+let nama="Vertin";
+let usia = 20
+usia = 21
+let y=2
+let isStudent = false;
+let buah = ["Apel", "Mangga","Jeruk"];
+let mahasiswa = {nama:"Rina", usia:20, jurusan:"Informatika"};
+
+console.log(nama);
+console.log(buah[1]);
+console.log('umur sy'+ usia)
+console.log(mahasiswa.nama);
+console.log(mahasiswa.jurusan);
